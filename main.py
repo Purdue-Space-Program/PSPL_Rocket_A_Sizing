1da75598c22f7eae6bab0d170c3d0d09e81d3ed4
David Z Gustafsson <david.z.gustafsson@gmail.com>
2025-07-08 14:04:26 +0200

    Implement numpy structured arrays for rocket array

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import scripts.engine as engine
 import coding_utils.constants as c
-import scripts.inputs as inputs
+import inputs as inputs
+import numpy as np
+import pandas as pd
 
-print (engine.find_L_star(inputs.FUEL_NAME, inputs.OXIDIZER_NAME))
+# print (engine.find_L_star(inputs.variables["FUEL_NAME"][1], inputs.constants["OXIDIZER_NAME"]))
+
+
+# Every combination (not sure if thats the right math term) of inputs will make a rocket.
+# Each of these rockets and its inputs will be stored as a dictionary in an n-dimensional array,
+# where n = # of variable inputs
+
+
